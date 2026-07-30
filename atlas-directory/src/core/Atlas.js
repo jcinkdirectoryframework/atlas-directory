05b765925741b23de7649f2087951e3ffb49232c
jcinkdirectoryframework <jcinkdirectoryframework@gmail.com>
2026-07-30 16:27:33 +0100

    Updated Atlas.js

--- atlas-directory/src/core/Atlas.js
+++ atlas-directory/src/core/Atlas.js
@@ -1,1 +1,36 @@
+/**
+ * Atlas
+ * A dependency-free, HTML-driven directory engine.
+ *
+ * @license MIT
+ */
 
+export default class Atlas {
+
+    /**
+     * Creates a new Atlas instance.
+     *
+     * @param {Object} options
+     */
+    constructor(options = {}) {
+
+        this.options = {
+            root: '[data-atlas]',
+            debug: false,
+            ...options
+        };
+
+        this.root = document.querySelector(this.options.root);
+
+        if (!this.root) {
+            throw new Error(
+                `Atlas could not find a root element matching "${this.options.root}".`
+            );
+        }
+
+        if (this.options.debug) {
+            console.info('Atlas initialised.', this);
+        }
+    }
+
+}
